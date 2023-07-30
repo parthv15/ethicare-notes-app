92e8677e7625f53a178b7eb4b7eec649eae340c0
parthv15 <parth.vyas523@gmail.com>
2023-07-30 14:12:08 +0930

    validation added + db connection + user model added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
+import "dotenv/config";
 import app from "./app";
+import mongoose from "mongoose";
+import env from "./util/validateEnv";
 
-const port = 1529;
+const port = env.PORT;
 
-app.listen(port, () => {
-  console.log(`[server]: listening on port ${port}`);
-});
+mongoose
+  .connect(env.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log("connected to mongodb");
+    app.listen(port, () => {
+      console.log(`[server]: listening on port ${port}`);
+    });
+  })
+  .catch(console.error);
