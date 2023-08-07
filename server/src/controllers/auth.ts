66f991d93355861690985728c4bbed90eac108d1
parthv15 <parth.vyas523@gmail.com>
2023-08-07 16:16:08 +0930

    auth impl added

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,5 +1,40 @@
 import { RequestHandler } from "express";
+import UserModel from "../models/user";
+import bcrypt from "bcrypt";
+import createHttpError from "http-errors";
 
-export const login: RequestHandler = (req, res) => {
-  res.status(200).json(req.body);
+interface loginBody {
+  email?: string;
+  password?: string;
+}
+
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  loginBody,
+  unknown
+> = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      throw createHttpError(400, "Parameters missing");
+    }
+
+    const user = await UserModel.findOne({ email })
+      .select("+name +password")
+      .exec();
+    if (!user) {
+      throw createHttpError(401, "Invalid Credentials");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw createHttpError(401, "Invalid Credentials");
+    }
+
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
