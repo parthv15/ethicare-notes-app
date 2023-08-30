3e4a7a24f2f246d2984697a5473bb92a9e6b3f99
parthv15 <parth.vyas523@gmail.com>
2023-08-30 21:48:17 +0930

    inline fix

--- client/src/components/Form/SingleOption.tsx
+++ client/src/components/Form/SingleOption.tsx
@@ -1,23 +1,20 @@
-
 type SingleOptionProps = {
-    question: string
-    name: string
-    options: string[]
-}
+  question: string;
+  name: string;
+  options: string[];
+};
 
-const SingleOptionProps = ({question, name, options}: SingleOptionProps) =>
-    (
-            <form className="bg-white justify-center px-8 mt-10">
-                <label className="content-center">
-                    {question}
-                </label><br/>
-                {options.map(item => (
-                    <div>                
-                        <input type="checkbox" className="mx-2" name={name}/>
-                        <label>{item}</label>
-                    </div>
-                ))}
-            </form>
-    );
+const SingleOptionProps = ({ question, name, options }: SingleOptionProps) => (
+  <form className="bg-white justify-center px-8 mt-10">
+    <label className="content-center">{question}</label>
+    <br />
+    {options.map((item) => (
+      <div>
+        <input type="checkbox" className="mx-2" name={name} />
+        <label>{item}</label>
+      </div>
+    ))}
+  </form>
+);
 
 export default SingleOptionProps;
